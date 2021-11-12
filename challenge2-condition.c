a2283c27c80883612167f64f09a21961ad470676
devtaha <tbouhdach2000@gmail.com>
2021-11-12 16:06:53 +0100

    push des exircice d'un condition

---
+++ challenge2-condition.c
@@ -0,0 +1,21 @@
+ #include <stdio.h>
+     
+      main (){
+
+           int nomber ;
+               printf("entrer un nomber:");
+               scanf("%d",&nomber);
+
+                if ( nomber%2 == 0){
+                     printf("cette nomber est pair");
+                }
+                  else  {
+                      printf("cette nomber est impair");
+                  }
+
+
+
+
+
+
+      }
---
+++ challenge3conditions.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+ #include <stdlib.h>  
+ #include <math.h>
+   void main (){
+        
+           char lettre ;
+           
+            printf("veuiller entrer un lettre =");
+            scanf("%c",&lettre);
+            switch(lettre){
+                case 'a':
+                         printf("est voiyalle");
+                         break;
+                case 'b' :
+                         printf("est voiyalle");
+                         break; 
+                 case 'i' :
+                         printf("est voiyalle");
+                         break;
+                 case 'e' :
+                         printf("est voiyalle");
+                         break;
+                case 'y' :
+                         printf("est voiyalle");
+                         break;
+                 case 'u' :
+                         printf("est voiyalle");
+                         break; 
+                default : printf("sory this character not existe");                                                 
+            }
+           
+           
+                
+
+          
+   }         
