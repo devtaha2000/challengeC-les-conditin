c01c4c9c6b3122ca3e8ef07b20c3062dc045a8f0
taha225624 <freelincertaha@gmail.com>
2021-11-13 22:47:01 +0100

    challenge le triplle

---
+++ challenge4-conditions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+     
+      main (){
+
+           int a , b , r;
+            printf("le premier nomber:");
+            scanf("%d",&a);
+            printf("le premier nomber:");
+            scanf("%d",&b);
+            r = (a+b)*3;
+            if( a == b )
+              printf("%d",r);
+              else printf( "les nombers entrer pas identique");
+             
+              
+         }
